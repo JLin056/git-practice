a818e99203e6ce2fb5df1f23742cdb8319681667
JLin <jlin890516@gmail.com>
2024-09-26 12:53:46 +0800

    fix:fib.js for迴圈內容

--- week-02/fib.js
+++ week-02/fib.js
@@ -1,18 +1,18 @@
 function fib(n) {
-    let first = 0; //設定第一個數字
-    let second = 1; //設定第二個數字
+    let first = 0; // 設定第一個數字
+    let second = 1; // 設定第二個數字
     let temp = first + second;
     if(n == 0){
         return first;
-    }else if(n == 1){
+    } else if(n == 1){
         return second;
-    }else{
-        for(i = 2;i <= n;i++){
+    } else{
+        for(let i = 2; i <= n; i++){
             temp += i
         }
         return temp;
     }
-  }
+}
   
   console.log(fib(0)); // 0
   console.log(fib(1)); // 1
