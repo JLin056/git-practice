e55ea27362ebce6e567116813a0aaf288b7a548a
JLin <jlin890516@gmail.com>
2024-09-27 15:07:11 +0800

    fixed:fix fib.js

--- week-02/fib.js
+++ week-02/fib.js
@@ -1,20 +1,11 @@
 function fib(n) {
-    let first = 0; // 設定第一個數字
-    let second = 1; // 設定第二個數字
-    let temp = first + second;
-    if(n == 0){
-        return first;
-    } else if(n == 1){
-        return second;
-    } else{
-        for(let i = 2; i <= n; i++){
-            temp += i
-        }
-        return temp;
-    }
+    if(n <= 1) {
+        return n;
+    } 
+    return fib(n - 1) + fib(n - 2);
 }
   
-  console.log(fib(0)); // 0
-  console.log(fib(1)); // 1
-  console.log(fib(5)); // 15
-  console.log(fib(10)); // 55
+console.log(fib(0)); // 0
+console.log(fib(1)); // 1
+console.log(fib(5)); // 5
+console.log(fib(10)); // 55
